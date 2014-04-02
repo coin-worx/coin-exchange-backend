cf461f0ae236f99e73497bcc54234114610f378c
Muhammad Bilal <bilal_78699@yahoo.com>
2014-04-02 12:07:16 +0500

    (COIN-59) Configured common logging using log4net.

--- src/common/CoinExchange.Rest.WebHost/App_Start/WebApiConfig.cs
+++ src/common/CoinExchange.Rest.WebHost/App_Start/WebApiConfig.cs
@@ -1,14 +1,16 @@
 ﻿using System.Linq;
 using System.Web.Http;
+using Common.Logging;
 
 namespace CoinExchange.Rest.WebHost.App_Start
 {
     public static class WebApiConfig
     {
+        //get current logger
+        private static ILog log = LogManager.GetCurrentClassLogger();
+         
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -33,6 +35,8 @@ namespace CoinExchange.Rest.WebHost.App_Start
             //return Json format
             var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
             config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+         
+            log.Info("Application Initialized.");
         }
     }
 }
