fb9c4a39bfec0cb89e72e34922802f2b9c04a8c0
Muhammad Bilal <bilal_78699@yahoo.com>
2014-04-14 12:46:19 +0500

    (COIN-167) created order id generator service, implementation and its spring config

---
+++ src/Trades/CoinExchange.Trades.Domain.Model/Services/IOrderIdGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinExchange.Trades.Domain.Model.Order;
+
+namespace CoinExchange.Trades.Domain.Model.Services
+{
+    public interface IOrderIdGenerator
+    {
+        OrderId GenerateOrderId();
+    }
+}
---
+++ src/Trades/CoinExchange.Trades.Infrastructure.Services/StubbedOrderIdGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoinExchange.Trades.Domain.Model.Order;
+using CoinExchange.Trades.Domain.Model.Services;
+
+namespace CoinExchange.Trades.Infrastructure.Services
+{
+    /// <summary>
+    /// Stub implementation of order id generator service
+    /// </summary>
+    public class StubbedOrderIdGenerator:IOrderIdGenerator
+    {
+        public OrderId GenerateOrderId()
+        {
+            return new OrderId((int)DateTime.Now.Ticks);
+        }
+    }
+}
